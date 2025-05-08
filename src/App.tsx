82470a57308f4c6746d47f14644f2cad9fabc386
durpdur <durpdur@gmail.com>
2025-05-08 16:29:39 -0700

    Render boxes according to viewport size

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,47 @@ import "./App.css";
 import WhiteBox from "./components/WhiteBox";
 
 function App() {
-  const [argPinkWhite, setPinkWhite] = useState(true); // Track the color state
+  const gridRow = Math.floor(window.innerWidth / 200);
+  const gridCol = Math.floor(window.innerHeight / 200);
 
-  const togglePinkWhite = () => {
-    let currColor = argPinkWhite ? "white" : "pink"; // logic
-    console.log(currColor);
+  // initial states
+  const initialStates: boolean[][] = Array.from(Array(gridRow), () =>
+    new Array(gridCol).fill(true)
+  );
 
-    setPinkWhite(prev => !prev); // Setter call
+  // state, setState
+  const [boxStates, setBoxStates] = useState<boolean[][]>(initialStates); // Track the color state
+
+  // function processing
+  const toggleStates = (i: number, j: number) => {
+    console.log(`Toggleing (${i}-${j})`);
+    const newStates = boxStates.map((row) => [...row]); // copies state
+    newStates[i][j] = !newStates[i][j]; // swap logic
+
+    setBoxStates(newStates); // sets mutated state
   };
 
-  return (
-    <>
-      <WhiteBox pinkWhite={argPinkWhite} onClick={togglePinkWhite} />
-    </>
+  //
+  const whiteBoxComponents = (
+    <div className="grid">
+      {
+        boxStates.map(
+          (row, i) =>
+            row.map(
+              (state, j) => (
+                <WhiteBox
+                  key={`${i}-${j}`}
+                  pinkWhite={state}
+                  onClick={() => toggleStates(i, j)}
+                />
+              ) // WhiteBox
+            ) // row.map
+        ) // boxStates.map
+      }
+    </div>
   );
+
+  return <>{whiteBoxComponents}</>;
 }
 
 export default App;
